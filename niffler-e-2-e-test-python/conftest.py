bb5522b7b5505ce02f4ddb3de1936bf1c5483b02
Shemetova Anastasiya <51288093+asya474@users.noreply.github.com>
2024-09-09 00:35:32 +0700

    aquintance_with_niffler branch

--- niffler-e-2-e-test-python/conftest.py
+++ niffler-e-2-e-test-python/conftest.py
@@ -1,81 +1,34 @@
-import os
-from faker import Faker
 import pytest
-from dotenv import load_dotenv
-from selene import browser, be
-from clients.spends_client import SpendsHttpClient
+from selene.support.shared import browser
+from faker import Faker
+from helper.create_user import create_user
+fake = Faker()
+
+@pytest.fixture(scope='function', autouse=True)
+def configure_browser():
+    browser.config.driver = 'Chrome'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.open('http://frontend.niffler.dc/')
+    browser.driver.maximize_window()
+    yield browser
+
+    browser.quit()
 
 
-@pytest.fixture(scope="session")
-def envs():
-    load_dotenv()
-
-
-@pytest.fixture(scope="session")
-def frontend_url(envs):
-    return os.getenv("FRONTEND_URL")
-
-
-@pytest.fixture(scope="session")
-def gateway_url(envs):
-    return os.getenv("GATEWAY_URL")
-
-
-@pytest.fixture(scope="session")
-def app_user(envs):
-    return os.getenv("TEST_USERNAME"), os.getenv("TEST_PASSWORD")
-
-
-@pytest.fixture(scope="function", autouse=True)
-def auth(frontend_url):
-    fake = Faker()
-    name = fake.first_name()
-    username, password = name, '12345'
-    browser.open(frontend_url)
-    # register
-    browser.element("a[href='http://auth.niffler.dc:9000/register']").click()
-    # create user
-    browser.element('#username').type(username)
-    browser.element('#password').type(password)
-    browser.element('#passwordSubmit').type(password)
-    # button sugn up
-    browser.element('button.form__submit').click()
-    # sign in
-    browser.element("a[href='http://frontend.niffler.dc/redirect']").click()
-    # auth
+@pytest.fixture(scope='function')
+def login_user():
+    user_credentials = create_user()
+    username=user_credentials['username']
+    password=user_credentials['password']
+    #login
     browser.element('[name="username"]').type(username)
     browser.element('[name="password"]').type(password)
     browser.element('button.form__submit').click()
-    #return {'username': username, 'password': password}
-    #return browser.driver.execute_script('return window.sessionStorage.getItem("id_token")')
 
 
-@pytest.fixture(scope="session")
-def spends_client(gateway_url, auth) -> SpendsHttpClient:
-    return SpendsHttpClient(gateway_url, auth)
+@pytest.fixture(scope='function')
+def user_credentials():
+    user_credentials = create_user()
 
-
-@pytest.fixture(params=[])
-def category(request, spends_client):
-    category_name = request.param
-    current_categories = spends_client.get_categories()
-    category_names = [category["category"] for category in current_categories]
-    if category_name not in category_names:
-        spends_client.add_category(category_name)
-    return category_name
-
-
-@pytest.fixture(params=[])
-def spends(request, spends_client):
-    spend = spends_client.add_spends(request.param)
-    yield spend
-    try:
-        # TODO вместо исключения проверить список текущих spends
-        spends_client.remove_spends([spend["id"]])
-    except Exception:
-        pass
-
-
-@pytest.fixture()
-def main_page(auth, frontend_url):
-    browser.open(frontend_url)
+    yield user_credentials  # Возвращаем данные пользователя для использования в тестах
