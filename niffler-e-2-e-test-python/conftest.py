8b32ca693e5f74f1932f3e7e52e5edcc625966cd
Shemetova Anastasiya <51288093+asya474@users.noreply.github.com>
2024-09-09 01:11:21 +0700

    aquintance_with_niffler branch

--- niffler-e-2-e-test-python/conftest.py
+++ niffler-e-2-e-test-python/conftest.py
@@ -2,7 +2,9 @@ import pytest
 from selene.support.shared import browser
 from faker import Faker
 from helper.create_user import create_user
+
 fake = Faker()
+
 
 @pytest.fixture(scope='function', autouse=True)
 def configure_browser():
@@ -19,9 +21,9 @@ def configure_browser():
 @pytest.fixture(scope='function')
 def login_user():
     user_credentials = create_user()
-    username=user_credentials['username']
-    password=user_credentials['password']
-    #login
+    username = user_credentials['username']
+    password = user_credentials['password']
+    # login
     browser.element('[name="username"]').type(username)
     browser.element('[name="password"]').type(password)
     browser.element('button.form__submit').click()
--- niffler-e-2-e-test-python/tests/test_start_page.py
+++ niffler-e-2-e-test-python/tests/test_start_page.py
@@ -6,12 +6,12 @@ from faker import Faker
 fake = Faker()
 username = fake.first_name()
 
-def test_start_page_authorization_button(frontend_url):
-    #browser.open(FRONTEND_URL)
+def test_start_page_authorization_button():
+    browser.open('http://frontend.niffler.dc/')
     browser.element("a[href='/redirect']").click()
     assert browser.element('p.form__paragraph').should(have.text("Please sign in"))
 
-def test_start_page_registration_button(frontend_url):
-    #browser.open(FRONTEND_URL)
+def test_start_page_registration_button():
+    browser.open('http://frontend.niffler.dc/')
     browser.element("a[href='http://auth.niffler.dc:9000/register']").click()
     assert browser.element('p.form__paragraph').should(have.text("Registration form"))
